fdf5ad0d5d2bb273b3de6da10e49a12a4d2845ab
NikolasSerg <nikolas.ser.a@yandex.ru>
2019-07-20 21:35:02 +0300

    almost finish

--- FL11_HW10/homework/js/fighters_game.js
+++ FL11_HW10/homework/js/fighters_game.js
@@ -13,32 +13,30 @@ function Fighter(name, damage, hp, agility) {
   this.getHealth = function() {
     return hp;
   };
-  this.setHealth = function(hpArg) {
-    return (hp -= hpArg);
-  };
-  this.atack = function(arg) {
-    // if (typeof arg === "object") {
-    //   console.log(arg);
-    //   console.log(arg.getName());
-    //   console.log(arg.getHealth());
-    //   console.log(arg.getDamage());
-    //   console.log(arg.getAgility());'
-    // }
-    // console.log(this.getHealth());
-
-    let randomAttack = Math.floor(Math.random() * 100);
-    console.log(randomAttack, " - ramdom");
-    if (randomAttack < 100 - arg.getAgility()) {
-      console.log(`Attack success`);
-      console.log(
-        `${this.getName()} make ${this.getDamage()} to ${arg.getName()}`
-      );
-      arg.setHealth(this.getDamage());
-      console.log(arg.getHealth());
-      win++;
+  // this.setDamageofHealth = function(hpArg) {
+  //   return hp - hpArg > 0 ? (hp = hp - hpArg) : (hp = 0);
+  // };
+  this.attack = function(arg) {
+    if (typeof arg === "object") {
+      let randomAttack = Math.floor(Math.random() * 100);
+      // console.log(randomAttack, " - ramdom");
+      if (randomAttack < 100 - arg.getAgility()) {
+        // console.log(`Attack success`);
+        console.log(
+          `${this.getName()} make ${this.getDamage()} damage to ${arg.getName()}`
+        );
+        // arg.setDamageofHealth(this.getDamage());
+        arg.dealDamage(this.getDamage());
+        console.log(`${arg.getName()} health ${arg.getHealth()}`);
+        this.addWin();
+        // win++;
+      } else {
+        console.log(`${this.getName()} attack missed`);
+        // loss++;
+        this.addLoss();
+      }
     } else {
-      console.log(`${this.getName()} attack missed`);
-      loss++;
+      console.log('argument must be object');
     }
   };
   this.logCombatHistory = function() {
@@ -49,25 +47,52 @@ function Fighter(name, damage, hp, agility) {
     console.log(hp, " - hp");
   };
   this.dealDamage = function(arg) {
-    hp - arg < 0 ? hp = 0 : hp -= arg;
-    console.log(hp, ' - hp');
+    // console.log(hp, " - hp");
+    return hp - arg < 0 ? (hp = 0) : (hp -= arg);
+
   };
   this.addWin = function() {
     win++;
-    console.log(win);
+    // console.log(win);
   };
   this.addLoss = function() {
     loss++;
-    console.log(loss);
+    // console.log(loss);
+  };
+}
+
+function battle(fighter, damager) {
+  if (fighter.getHealth() === 0) {
+    console.log(`${fighter.getName()} is dead and can't fight`);
+    return;
+  } else if (damager.getHealth() === 0) {
+    console.log(`${damager.getName()} is dead and can't fight`);
+    return;
+  } else {
+    console.log(`battle start`);
+    for (let i = 0; fighter.getHealth() > 0 || damager.getHealth() > 0; i++) {
+      if (fighter.getHealth() === 0 || damager.getHealth() === 0) {
+        console.log(
+          `battle finish ${
+            fighter.getHealth() === 0 ? fighter.getName() : damager.getName()
+          } health 0`
+        );
+        break;
+      }
+      fighter.attack(damager);
+      if (fighter.getHealth() === 0 || damager.getHealth() === 0) {
+        console.log(
+          `battle finish ${
+            fighter.getHealth() === 0 ? fighter.getName() : damager.getName()
+          } health 0`
+        );
+        break;
+      }
+      damager.attack(fighter);
+      console.log('------------------------------------------------');
+    }
   }
 }
 
-
-
-
 let myFighter = new Fighter("Jhon", 20, 100, 25);
 let damager = new Fighter("Andru", 30, 70, 40);
-let damager2 = new Fighter("Su", 70, 100, 5);
-
-// name, damage, hp, agility
-myFighter.atack(damager);
